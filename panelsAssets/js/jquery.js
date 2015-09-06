a581953398e86490a22164b2284267d9dd78ee82
klipspringr <mari.tir3sias@gmail.com>
2015-09-06 01:05:23 -0400

    Added Search screen.

---
+++ panelsAssets/js/jquery.js
@@ -0,0 +1,25 @@
+$(function() {
+  var d = new Date();
+  var weekday = new Array(7);
+  weekday[0]=  "Sunday";
+  weekday[1] = "Monday";
+  weekday[2] = "Tuesday";
+  weekday[3] = "Wednesday";
+  weekday[4] = "Thursday";
+  weekday[5] = "Friday";
+  weekday[6] = "Saturday";
+  var day = weekday[d.getDay()];
+  $("#weekday").text(day);
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top-68
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
